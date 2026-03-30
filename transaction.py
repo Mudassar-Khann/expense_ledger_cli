4787fe0f44b422e9552c2fd022d84371b6c1ea29
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-03-30 23:21:45 +0500

    feat: add Transaction class to represent financial transactions

---
+++ transaction.py
@@ -0,0 +1,10 @@
+class Transcation:
+
+    def __init__(self, amount, category, description, date):
+        self.amount = amount
+        self.category = category
+        self.description = description
+        self.date= date
+
+
+
