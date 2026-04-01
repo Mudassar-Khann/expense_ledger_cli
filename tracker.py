a709164e9b25ac0a6400ece87f43fd882eac6f76
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-01 20:23:29 +0500

    feat: Implement basic structure of ExpenseTracker class with method.

--- tracker.py
+++ tracker.py
@@ -1,12 +1,19 @@
+class ExpenseTracker:
 
-def add():
-    pass
+    def __init__(self):
+        pass
+
+    def add_transiction(self, amount, category, discription):
+        pass
+
+
+    def list_transactions(self):
+        pass
+
+    def search_transactions(self, word):
+        pass
 
 
 
-def search():
-    pass
 
 
-def list():
-    pass
