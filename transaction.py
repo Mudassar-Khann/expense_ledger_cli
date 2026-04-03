40b9e5636481cf582f16514ca1c7c6b691914aab
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-03 09:13:30 +0500

    feat: add to_dict and __repr__ methods to Transaction class

--- transaction.py
+++ transaction.py
@@ -4,7 +4,25 @@ class Transcation:
         self.amount = amount
         self.category = category
         self.description = description
-        self.date= date
+        self.date = date
+
+
+    def to_dict(self):
+        return {
+            "amount" : self.amount,
+            "category" : self.category,
+            "description" : self.description,
+            "date" : self.date
+        }
+
+
+    def __repr__(self):
+        return f"Transaction({self.amount}, {self.category}, {self.description}, {self.date})"
+
+
+
+
+
 
 
 
