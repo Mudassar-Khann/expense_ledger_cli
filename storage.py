5822675d30cf339cf8d000e0ce5460d4d1b1a4cf
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-06 14:48:41 +0500

    feat: add load function to load transactions from JSON file and return as list of dictionaries.

--- storage.py
+++ storage.py
@@ -2,11 +2,22 @@ import json
 
 
 
+
 def save(transactions: list):
 
     data = [i.to_dict() for i in transactions ]
     with open("data/transactions.json", "w", encoding="utf-8") as f:
           json.dump(data, f, indent=5)
+
+
+
+
+
+def load():
+     with open("data/transactions.json", "r", encoding="utf-8") as f:
+         data = json.load(f)
+         return data
+
 
 
 
