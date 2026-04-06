7de9fa3846c4b075b708b7dbca390f9bfab509b0
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-06 17:47:44 +0500

    style: improved the naming and readabilty.

--- transaction.py
+++ transaction.py
@@ -1,35 +1,24 @@
 from datetime import datetime, timezone
 
-class Transcation:
 
+class Transaction:
     def __init__(self, amount, category, description, date=None):
         self.amount = amount
         self.category = category
         self.description = description
         self.date = datetime.now(timezone.utc).isoformat() if date is None else date
 
-
     def readable_date(self):
         dt = datetime.fromisoformat(self.date).astimezone()
         return dt.strftime("%d %b %Y, %I:%M %p")
 
-
     def to_dict(self):
         return {
-            "amount" : self.amount,
-            "category" : self.category,
-            "description" : self.description,
-            "date" : self.date
+            "amount": self.amount,
+            "category": self.category,
+            "description": self.description,
+            "date": self.date
         }
 
-
     def __repr__(self):
-        return f"Transaction(amount={self.amount}, category={self.category}, description={self.description}, date={self.date})"
-
-
-
-
-
-
-
-
+        return f"Transaction({self.amount}, {self.category}, {self.description})"
