cd54e16c55ab4f80c500a43a92c987d540e00c70
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-06 16:05:53 +0500

    feat: add log error function to logger.py and improve log info function to include more details about transactions

--- logger.py
+++ logger.py
@@ -3,9 +3,14 @@ import logging, os
 os.makedirs("logs", exist_ok=True)
 
 
-def log_info(input):
-    logging.basicConfig(filename="logs/app.log", level=logging.INFO
-    format=f"[%(levelname)s]" {input})
+def log_info(messsage, amount=None,  category=None, description=None, result=None):
+    logging.basicConfig(filename="logs/app.log", level=logging.INFO,
+    format=f"[%(levelname)s] {messsage} Added Transaction {amount} {category} {description}")
+
+def log_error(input):
+    logging.basicConfig(filename="logs/app.log", level=logging.ERROR,
+    format=f"[%(asctime)s] [%(levelname)s] Invalid amount input: {input}")
+
 
 
 
