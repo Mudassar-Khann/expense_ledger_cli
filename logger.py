14ce3905dacad750cd12621b57b00fa5b2783beb
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-06 21:44:26 +0500

    fixed: Refactor logging setup for using using repeated basicConfig calls and improve log message formatting.

--- logger.py
+++ logger.py
@@ -1,16 +1,13 @@
-import logging, os
+import logging
+import os
 
 os.makedirs("logs", exist_ok=True)
 
-
-def log_info(messsage, amount=None,  category=None, description=None, result=None):
-    logging.basicConfig(filename="logs/app.log", level=logging.INFO,
-    format=f"[%(levelname)s] {messsage} Added Transaction {amount} {category} {description}")
-
-def log_error(input):
-    logging.basicConfig(filename="logs/app.log", level=logging.ERROR,
-    format=f"[%(asctime)s] [%(levelname)s] Invalid amount input: {input}")
-
+logging.basicConfig(
+    filename="logs/app.log",
+    level=logging.INFO,
+    format="[%(asctime)s] [%(levelname)s] %(message)s"
+)
 
 
 
