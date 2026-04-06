0d38078c3dc0fd1e89b48020a71699ee412e31ce
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-06 17:43:37 +0500

    refactored: improved logic and and handled error incase of file dosn't exist

--- storage.py
+++ storage.py
@@ -1,25 +1,20 @@
 import json
+import os
+
+FILE = "data/transactions.json"
 
 
+def save(transactions):
+    os.makedirs("data", exist_ok=True)
 
-
-def save(transactions: list):
-
-    data = [i.to_dict() for i in transactions ]
-    with open("data/transactions.json", "w", encoding="utf-8") as f:
-          json.dump(data, f, indent=5)
-
-
-
+    data = [t.to_dict() for t in transactions]
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
 
 
 def load():
-     with open("data/transactions.json", "r", encoding="utf-8") as f:
-         data = json.load(f)
-         return data
+    if not os.path.exists(FILE):
+        return []
 
-
-
-
-
-
+    with open(FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
